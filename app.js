aa9b7738046dc98c81fa77f175147560d534a0a7
Sirwin <sirwin2win@gmail.com>
2025-11-05 12:49:03 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Hello World!")
+//  Syntax is guiding rule
+// var, const, let
+/*
+Variable is a container for storing values.
+Variable scope: talking about where the variable was declared
+Best ways of choosing variable names
+1. Camel Case: firstName, lastName
+2. Pascal Case: MiddleName, LastName
+3. Snake Case: first_name, last_name
+*/
+const name = "John Doe"
+const double = function(n){
+    let a = 2
+    console.log(n*a)
+}
+double(5)
